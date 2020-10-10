e60d364c2cbf676fb421292a7e84014fca477d24
Sarah <sarah.pfaeffli@akros.ch>
2020-10-10 10:16:03 +0200

    Abschnitt 3:Dependency Injection with Spring ------------------------------------ 35: Dependency Injection using Spring Framework ------------ - create beans with spring

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
   private final GreetingService greetingService;
 
+  // @Autowired annotation nicht notwendig --> spring findet das automatisch!
   public ConstructorInjectedController(GreetingService greetingService) {
     this.greetingService = greetingService;
   }
--- src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -1,11 +1,8 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.GreetingService;
-import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import guru.springframework.sfgdi.services.ConstructorGreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PropertyInjectedControllerTest {
 
@@ -14,7 +11,7 @@ class PropertyInjectedControllerTest {
   @BeforeEach
   void setup() {
     controller = new PropertyInjectedController();
-    controller.greetingService = new GreetingServiceImpl();
+    controller.greetingService = new ConstructorGreetingServiceImpl();
   }
 
   @Test
